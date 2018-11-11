00d3532a67817ec86d751cad73e0c477036a36c4
mundo_luna <raymundo167@gmail.com>
2018-11-10 23:24:02 -0800

    added material

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import App from './app'
 import registerServiceWorker from './registerServiceWorker';
 
 // Reset all CSS settings in browsers
-import "./styles/importer.css";
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
--- src/routing/routes.js
+++ src/routing/routes.js
@@ -3,15 +3,12 @@ import { Redirect, Route, Switch } from "react-router-dom";
 
 import pages from "./pages";
 
-import Layout from "../app/common/Layout";
-
 import Home from "../app/Home/Home.js";
 
 // Must construct an object out of render() function for
 // both performance and the correctness of module states, i.e. this.state.
 
 export default () => (
-	<Layout>
 		<Switch>
 			<Route exact path="/" component={Home} />
 
@@ -27,5 +24,4 @@ export default () => (
 				))}
 			<Redirect to="/" />
 		</Switch>
-	</Layout>
 );
