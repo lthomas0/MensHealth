33e04c2a87282e547720ce19d6f2493cb21323aa
mundo_luna <raymundo167@gmail.com>
2018-11-10 23:30:13 -0800

    deleted featured posts from home

--- src/app/Home/Home.js
+++ src/app/Home/Home.js
@@ -88,22 +88,6 @@ const sections = [
   'Travel',
 ];
 
-const featuredPosts = [
-  {
-    title: 'Featured post',
-    date: 'Nov 12',
-    description:
-      'This is a wider card with supporting text below as a natural lead-in to additional content.',
-  },
-  {
-    title: 'Post title',
-    date: 'Nov 11',
-    description:
-      'This is a wider card with supporting text below as a natural lead-in to additional content.',
-  },
-];
-
-
 const archives = [
   'March 2020',
   'February 2020',
